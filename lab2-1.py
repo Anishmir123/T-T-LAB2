a0f33b4a144fc4f2a79c62ade142cf20a06fd9e5
Anish Mir <114848782+Anishmir123@users.noreply.github.com>
2023-01-31 12:55:42 +0530

    Update lab2-1.py

--- lab2-1.py
+++ lab2-1.py
@@ -1,3 +1,6 @@
+//WAP to determine whether a person is eligible to cast vote or not. If he /she was not
+eligible display how many years left to be eligible.
+
 age = int(input("Enter your age: "))
 
 if (age>=18):
